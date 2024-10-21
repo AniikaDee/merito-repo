a1f38ddd349609f3e279f7d8333b0acec650a13e
nazwa <bla@bla.pl>
2024-10-21 10:56:53 +0200

    pierwsza wersja aplikacji dotnet

---
+++ MyDotNetWebbApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyDotNetWebbApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
